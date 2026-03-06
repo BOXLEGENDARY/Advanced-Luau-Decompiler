d23f1c7551c62e6412a28dc5cda14c7f6ca90379
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2026-03-07 06:28:56 +0700

    Add files via upload

--- Strings.lua
+++ Strings.lua
@@ -1,6 +1,6 @@
 local BaseStrings = {
-    os.date("%Y-%m-%d %H:%M:%S"),
-    "-- Decompiler By Advanced Luau Decompiler\n",
+    "Decompiler By Advanced Luau Decompiler",
+    os.date("%Y-%m-%d %H:%M:%S\n"),
 }
 
 local CombinedBaseString = table.concat(BaseStrings, "\n")
