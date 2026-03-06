437985cbab5219957efd8befa2aafff73ac3aa59
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2026-03-07 06:10:31 +0700

    Add files via upload

--- Strings.lua
+++ Strings.lua
@@ -6,12 +6,12 @@ local BaseStrings = {
 local CombinedBaseString = table.concat(BaseStrings, "\n")
 
 local Strings = {
-    SUCCESS                = ("-- %s\n%s"):format(CombinedBaseString, "%s"),
-    TIMEOUT                = "-- DECOMPILER TIMEOUT",
-    COMPILATION_FAILURE    = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
-    UNSUPPORTED_LBC_VERSION= "-- PASSED BYTECODE IS TOO OLD OR UNSUPPORTED",
-    USED_GLOBALS           = "-- USED GLOBALS: %s.\n\n",
-    DECOMPILER_REMARK      = "-- DECOMPILER REMARK: %s\n"
+    SUCCESS = ("-- %s\n%s"):format(CombinedBaseString, "%s"),
+    TIMEOUT = "-- DECOMPILER TIMEOUT",
+    COMPILATION_FAILURE = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
+    UNSUPPORTED_LBC_VERSION = "-- PASSED BYTECODE IS TOO OLD OR UNSUPPORTED",
+    USED_GLOBALS = "-- USED GLOBALS: %s.\n\n",
+    DECOMPILER_REMARK = "-- DECOMPILER REMARK: %s\n"
 }
 
 return Strings
